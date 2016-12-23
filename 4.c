5cd026d6689dd88547ca49c461dd38b8afbb41e3
vishnuvardhan1910 <vishnuvardhan1910@gmail.com>
2016-12-23 01:02:49 -0800

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number:");
+scanf("%d");
+if(num%2==0)
+{
+printf("the number is even");
+}
+else
+printf("the number is odd");
+return 0;
+}

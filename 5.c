47fc9413b7e72c462db08c756d1620f248363177
vishnuvardhan1910 <vishnuvardhan1910@gmail.com>
2016-12-23 01:09:13 -0800

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("enter the alphabet");
+scanf("%c",&ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+printf("the alphabet is vowel");
+}
+else
+printf("the alphabet is constant");
+}

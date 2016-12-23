76238c781d8e6a3a94d385fbb8acfde4d58678b2
vishnuvardhan1910 <vishnuvardhan1910@gmail.com>
2016-12-23 00:55:15 -0800

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+int main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if(num==0)
+{
+printf("the number is zero");
+}
+else if(num>0)
+printf("the number is positive");
+else
+printf("the number is negevite");
+return 0;
+}
